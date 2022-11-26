77fdda0a619e6c9c585a2c18aa2bd94617f35d5a
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-11-26 04:15:05 -0800

    created components for the details page

---
+++ src/DetailsComponent/Hero.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+
+export default function DetailsHero({id}) {
+
+  let [project, setProject] = useState([]);
+
+    useEffect(() => {
+        getProject()
+    }, [])
+    
+  
+  let getProject = async () => {
+    let response = await fetch(`http://127.0.0.1:8000/api/projects/${id}`);
+    let data = await response.json();
+    setProject(data);
+  };
+  
+
+  return (
+    <div className="container">
+      <div className="details">
+        <h1>{project.title}</h1>
+
+        <p>{project.description}</p>
+
+        <div className="btns det-btn">
+          <a href="me.com">
+            <button>Not Deployed</button>
+          </a>
+          <a href={project.github_link}>
+            <button>View Code</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/DetailsComponent/SlideShow.js
@@ -0,0 +1,11 @@
+const SlideShow = () => {
+    return (
+        <div className="container">
+            <div>
+                <img src="../images/cycom.png" alt="project banner img" className="slideshow-img" />
+            </div>
+        </div>
+    )
+}
+
+export default SlideShow
