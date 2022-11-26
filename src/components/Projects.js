c19a7452384218bd0d61333a40bcafcb652a3e9e
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-11-25 23:14:06 -0800

    Added components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,63 +1,38 @@
+import React, {useState, useEffect} from 'react'
+
 export default function Projects() {
+  
+  let [projects, setProject] = useState([])
+  useEffect(() => {
+    getProject()
+  })
+
+  let getProject = async () => {
+    let response = await fetch("http://127.0.0.1:8000/api/projects/");
+    let data = await response.json();
+    setProject(data);
+  };
+
   return (
     <section className="container projects">
       <h1>Projects</h1>
+
       <div className="grid-container">
-        <a href="me.com">
-          <div className="card">
-            <img src="./images/me.png" className="card-image" alt="card-img" />
-            <h2>My Portfolio</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem quisquam id sequi!
-            </p>
-          </div>
-        </a>
-
-        <a href="me.com">
-          <div className="card">
-            <img
-              src="./images/ig_soc.png"
-              className="card-image"
-              alt="card-img"
-            />
-            <h2>Social Media</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem quisquam id sequi!
-            </p>
-          </div>
-        </a>
-
-        <a href="me.com">
-          <div className="card">
-            <img
-              src="./images/cycom.png"
-              className="card-image"
-              alt="card-img"
-            />
-            <h2>Cycom</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem quisquam id sequi!
-            </p>
-          </div>
-        </a>
-
-        <a href="me.com">
-          <div className="card">
-            <img
-              src="./images/form.png"
-              className="card-image"
-              alt="card-img"
-            />
-            <h2>Application Form</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem quisquam id sequi!
-            </p>
-          </div>
-        </a>
+        {projects.map((project, index) => (
+          <a href="me.com" key={index}>
+            <div className="card">
+              <img
+                src={project.image}
+                className="card-image"
+                alt="card-img"
+              />
+              <h2>{project.title}</h2>
+              <p>
+                {project.description}
+              </p>
+            </div>
+          </a>
+        ))}
       </div>
     </section>
   );
