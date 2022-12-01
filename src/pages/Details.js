15e1ae5438995b38c5848caf9e7d3175bd9183f8
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-12-01 00:51:09 -0800

    added slideshow component

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,14 +1,17 @@
 import DetailsHero from "../DetailsComponent/Hero";
 import SlideShow from "../DetailsComponent/SlideShow";
+import TechStack from "../DetailsComponent/TechStack";
 import { useParams } from "react-router-dom";
 
-
 export default function Details() {
-    let {id} = useParams()
-    return (
-      <>
-        <DetailsHero id={id} />
-        <SlideShow/>
-      </>
-    );
+  let { id } = useParams();
+  
+
+  return (
+    <>
+      <DetailsHero id={id} />
+      <SlideShow />
+      <TechStack />
+    </>
+  );
 }
