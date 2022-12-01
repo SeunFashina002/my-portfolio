f8d82a76a547030c3c798428949197481bcbf9ef
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-12-01 00:39:28 -0800

    initial commit

---
+++ src/DetailsComponent/TechStack.js
@@ -0,0 +1,52 @@
+const TechStack = () => {
+  return (
+    <div className="container">
+      <div className="project-stack-div">
+        <h1>Tech Stack</h1>
+        <div className="skills">
+          <h2>Front End</h2>
+          <div className="skill-grid">
+            <div className="logo_group">
+              <img
+                className="logo_stack"
+                src="./pr_logo/logo_py.png"
+                alt="python"
+              />
+              <p className="stack_name">Python</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills">
+          <h2>Back End</h2>
+          <div className="skill-grid">
+            <div className="logo_group">
+              <img
+                className="logo_stack"
+                src="./pr_logo/logo_py.png"
+                alt="python"
+              />
+              <p className="stack_name">Python</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills">
+          <h2>Others</h2>
+          <div className="skill-grid">
+            <div className="logo_group">
+              <img
+                className="logo_stack"
+                src="./pr_logo/logo_py.png"
+                alt="python"
+              />
+              <p className="stack_name">Python</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
