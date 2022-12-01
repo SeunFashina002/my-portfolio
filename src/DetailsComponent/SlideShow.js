0349c86dc87f28a8cbe012078bbddb2c369e08fd
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-12-01 00:47:48 -0800

    added a slide show functionality in the project details page

--- src/DetailsComponent/SlideShow.js
+++ src/DetailsComponent/SlideShow.js
@@ -1,11 +1,61 @@
-const SlideShow = () => {
-    return (
-        <div className="container">
-            <div>
-                <img src="../images/cycom.png" alt="project banner img" className="slideshow-img" />
-            </div>
-        </div>
-    )
+import React, { useEffect,  useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade } from 'swiper'
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+
+
+import "../DetailsComponent/slidestyle.css"
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function SlideShow() {
+    let [slideshows, setSlideshows] = useState([])
+
+    useEffect(() => {
+        getSlideShows()
+    }, [])
+
+    let getSlideShows = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/projects/detail/slideshows')
+        let data = await response.json()
+        setSlideshows(data);
+    }
+
+
+
+  return (
+    <div className="container">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        effect={'fade'}
+        speed={800}
+        modules={[Autoplay, Pagination, Navigation, EffectFade]}
+        className="mySwiper"
+      >
+        {slideshows.map((slideshow, index) => (
+          <SwiperSlide>
+            <img key={index} src={slideshow.image} alt={slideshow.alt} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
-export default SlideShow
