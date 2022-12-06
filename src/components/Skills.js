97ffe026b87de6558d18601f0bba38105ca94418
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-12-06 03:12:23 -0800

    retrieved the stack from the database

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,75 +1,34 @@
+import { useEffect, useState } from "react";
 export default function Skills() {
+    useEffect(() => {
+      getStack();
+    }, []);
+
+  
+    let [stacks, setStacks] = useState([])
+    let getStack = async () => {
+      let response = await fetch("http://127.0.0.1:8000/api/stacks/");
+      let data = await response.json();
+      setStacks(data);
+    };
+
+
+
     return (
       <section className="container">
         <div className="skills">
           <h1>Skills</h1>
           <div className="skill-grid">
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/logo_py.png"
-                alt="python"
-              />
-              <p className="stack_name">Python</p>
-            </div>
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/django_logo.png"
-                alt="python"
-              />
-              <p className="stack_name">Django</p>
-            </div>
-
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/rest_logo.png"
-                alt="python"
-              />
-              <p className="stack_name">Rest Framework</p>
-            </div>
-                    
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/javascript__logo.svg"
-                alt="python"
-              />
-              <p className="stack_name">JavaScript</p>
-            </div>
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/logos_react.svg"
-                alt="python"
-              />
-              <p className="stack_name">React</p>
-            </div>
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/html.svg"
-                alt="python"
-              />
-              <p className="stack_name">HTML</p>
-            </div>
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/css.svg"
-                alt="python"
-              />
-              <p className="stack_name">CSS</p>
-            </div>
-            <div className="logo_group">
-              <img
-                className="logo_stack"
-                src="./pr_logo/tailwind.svg"
-                alt="python"
-              />
-              <p className="stack_name">Tailwind CSS</p>
-            </div>
+            {stacks.map((stack, index) => (
+              <div className="logo_group">
+                <img
+                  className="logo_stack"
+                  src={stack.stack_image}
+                  alt={stack.alt}
+                />
+                <p className="stack_name">{stack.stack_name}</p>
+              </div>
+            ))}
           </div>
         </div>
       </section>
