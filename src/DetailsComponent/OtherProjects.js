949084d0ad4a397a29f87534044863f7983cb954
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-12-07 01:39:44 -0800

    added a section for other projects to be displayed

---
+++ src/DetailsComponent/OtherProjects.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+
+const OtherProejcts = ({id}) => {
+    let [projects, setProjects] = useState([]);
+        useEffect(() => {
+        getProjects();
+    }, []);
+
+        let getProjects = async () => {
+        let response = await fetch("http://127.0.0.1:8000/api/projects/");
+        let data = await response.json();
+        setProjects(data);
+    };
+    const other_proejcts = projects.map((project, index) => {
+        if (id !== project.id) {
+            return (
+              <div className="others">
+                <a href={`/projects/${project.id}`} key={index}>
+                  <img
+                    src={project.image}
+                    className="others-image"
+                    alt="card-img"
+                  />
+                </a>
+
+                <h2>
+                  <a href={`/projects/${project.id}`}>{project.title}</a>
+                </h2>
+                <p>
+                  <a href={`/projects/${project.id}`}>{project.description}</a>
+                </p>
+              </div>
+            );
+        }
+    })
+    return (
+    <section className="container projects">
+        <h1>Other Projects</h1>
+
+        <div className="grid-container">
+            {other_proejcts}
+        </div>
+    </section>
+    );
+
+};
+
+export default OtherProejcts
