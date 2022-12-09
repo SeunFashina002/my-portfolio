e6145814e7bb319c0fdf5187e6c156cb48b75e18
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-12-09 05:56:13 -0800

    added an anchor tag to the name in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,7 @@
 export default function Footer() {
-    return <div className='footer'>Designed by Kelvin</div>;
+    return (
+      <div className="footer">
+        Designed by <a href="https://portfolio-site-ecru.vercel.app">Kelvin</a>
+      </div>
+    );
 }
