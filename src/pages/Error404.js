40937b43ff0972f1534f066911af4205405b3a89
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-17 15:10:04 -0700

    fixed 404 not found

---
+++ src/pages/Error404.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export const NotFound = () => {
+  return (
+    <div className="notfound">
+      <div className="body">
+        <img src="./images/404.svg" alt="not found image" />
+        <div className="error-message">
+          <h3>404 | Sorry, this is the end of the road!</h3>
+          <Link to="/">&#8592; Go back to home</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
