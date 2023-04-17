4dc4e55fa808d2de24eaefaa7b7fcf69a9bcd726
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-17 15:13:41 -0700

    refactored the code to be more efficient

--- src/DetailsComponent/TechStack.js
+++ src/DetailsComponent/TechStack.js
@@ -1,64 +1,48 @@
-import {useEffect, useState} from 'react'
-import { BASE_URL } from '../constants/Base_url';
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../constants/Base_url";
+
 const TechStack = ({ id }) => {
-  let [stacks, setStacks] = useState([])    
-  let [front_tech_stack, setFrontTechStack] = useState('Frontend')
+  const [stacks, setStacks] = useState([]);
 
   useEffect(() => {
-    getStack()
-  }, [])
+    getStack();
+  }, []);
 
   const getStack = async () => {
     let response = await fetch(`${BASE_URL}/${id}/detail/pstack`);
-    let data = await response.json()
-    setStacks(data)
-  }
+    let data = await response.json();
+    setStacks(data);
+  };
 
-  
+  // extract categories from the stack and store in new array
+  const categories = Array.from(new Set(stacks.map(stack => stack.category)));
+
   return (
     <div className="container">
       <div className="project-stack-div">
         <h1>Tech Stack</h1>
-        <div className="skills">
-          <h1>{front_tech_stack}</h1>
-          <div className="skill-grid">
-            {stacks.map((stack, index) => {
-              if (stack.category == "frontend") {
-                return (
-                  <div key={index} className="logo_group">
-                    <img
-                      className="logo_stack"
-                      src={stack.stack_image}
-                      alt={stack.alt}
-                    />
-                    <p className="stack_name">{stack.stack_name}</p>
-                  </div>
-                );
-              }
-            })}
+        {/* loop thru the categories arrays we created from stack */}
+        {categories.map(category => (
+          <div key={category} className="skills">
+            <h1>{category}</h1>
+            <div className="skill-grid">
+              {stacks.map(stack => {
+                if (stack.category === category) {
+                  return (
+                    <div key={stack.id} className="logo_group">
+                      <img
+                        className="logo_stack"
+                        src={stack.stack_image}
+                        alt={stack.alt}
+                      />
+                      <p className="stack_name">{stack.stack_name}</p>
+                    </div>
+                  );
+                }
+              })}
+            </div>
           </div>
-        </div>
-
-        
-        <div className="skills">
-          <h1>Backend</h1>
-          <div className="skill-grid">
-            {stacks.map((stack, index) => {
-              if (stack.category == "backend") {
-                return (
-                  <div key={index} className="logo_group">
-                    <img
-                      className="logo_stack"
-                      src={stack.stack_image}
-                      alt={stack.alt}
-                    />
-                    <p className="stack_name">{stack.stack_name}</p>
-                  </div>
-                );
-              }
-            })}
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
