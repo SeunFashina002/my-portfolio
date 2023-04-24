001914f58a4a34ba4ea12846a425da52fd22a2c1
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-24 06:38:40 -0700

    creatd a custom 404 page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -4,7 +4,7 @@ export const NotFound = () => {
   return (
     <div className="notfound">
       <div className="body">
-        <img src="./images/404.svg" alt="not found image" />
+        <img src="/images/404.svg" alt="not found image" />
         <div className="error-message">
           <h3>404 | Sorry, this is the end of the road!</h3>
           <Link to="/">&#8592; Go back to home</Link>
