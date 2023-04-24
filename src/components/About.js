fb341390dc518c76e129e6a8da70de5acf419570
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-24 06:36:29 -0700

    updated my about

--- src/components/About.js
+++ src/components/About.js
@@ -4,27 +4,27 @@ export default function About() {
       <div className="about">
         <h2>About</h2>
         <p>
-          I am a versatile web developer with experience in both frontend and
-          backend stacks. I have a passion for building high-quality,
-          user-friendly applications that deliver exceptional experiences to
-          end-users. While I have a good foundation in frontend development,
-          learning Django made me love backend development. I decided to hone my
-          skills in backend development and building APIs, which enables me to
-          deliver end-to-end solutions for clients. Additionally, I have been
-          sharpening my backend development skills with Django, enabling me to
-          build robust and scalable APIs that power the applications I develop.
-          My dedication to creating efficient and effective applications has
-          earned me a reputation as a reliable developer, capable of working
-          collaboratively to deliver projects on time and on budget. I pride
-          myself on staying up-to-date with the latest industry trends and
-          technology, which allows me to deliver cutting-edge solutions that
-          exceed client expectations. Recently, I started my research and
-          learning of mobile applications development using React Native. The
-          fact that APIs can be consumed with one codebase on different mobile
-          platforms using React Native, thrills me. If you're looking for a
-          developer who can bring your ideas to life and deliver exceptional
-          results, I am confident that I have the skills and experience you
-          need. Let's work together to create the next great application.
+          As a Python backend developer, I have a passion for building robust
+          and scalable applications that can handle large amounts of data and
+          traffic. However, I also have experience in frontend development,
+          including HTML, CSS, and JavaScript, as well as modern frameworks like
+          React.
+        </p>
+        <p>
+          Things that sets me apart from other developers is my experience in
+          contributing to both backend and frontend projects. This has given me
+          a unique perspective on the development process, allowing me to
+          collaborate seamlessly with other backend developers to provide API's
+          that meets the full need of the frontend developers enabling them
+          build applications that are not only powerful and efficient, but also
+          user-friendly and visually appealing.
+        </p>
+
+        <p>
+          I am always looking for new challenges and opportunities to expand my
+          knowledge and skills in Python backend development.If you are looking
+          for a developer who is passionate about building high-quality,
+          scalable applications, I would love to hear from you. 
         </p>
       </div>
     </section>
